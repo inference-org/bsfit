fc595b039119cb63ce64e14ddd00aebfca1c440e
steeve <steeve@stanford.edu>
2022-03-23 17:01:01 +0100

    Comment to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# [TODO]: It currently does not clean up 
+# deprecated files in source/_autosummary
+# by itself.
 
 # build and deploy docs
 cd docs/source;
@@ -7,5 +10,7 @@ make html;
 cd ../..;
 
 # deploy docs/source/ to docs/
-cp -a docs/source/_build/html/. docs/ # copy to deploy path
-touch docs/.nojekyll # ignore jekyll on github
+# - copy to deploy path
+# - ignore jekyll on github
+cp -a docs/source/_build/html/. docs/ 
+touch docs/.nojekyll 
