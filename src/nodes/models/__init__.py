f0386cc2d3fef1595589692ee1816d254dcde746
steeve <steeve@stanford.edu>
2022-03-23 15:16:35 +0100

    Drop old api

--- src/nodes/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# note: the doctsring code below within
-# """ is converted to a restructuredText
-# .rst file by sphinx to automatically
-# generate the api's documentation
-#
-# docstring style used: Google style
-"""
-Copyright 2022 by Steeve Laquitaine, GNU license 
-"""
