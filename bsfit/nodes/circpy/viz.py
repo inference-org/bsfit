a8e6aac9b7104378999ea4e623d0f59c1d08248c
steeve <steeve@stanford.edu>
2022-03-31 13:12:08 +0200

    create circpy package

---
+++ bsfit/nodes/circpy/viz.py
@@ -0,0 +1,15 @@
+import numpy as np
+from bsfit.nodes.circpy.data import VonMises
+from matplotlib import pyplot as plt
+
+
+def plot_von_mises(support_space:np.ndarray, k_vm:float):
+
+    # calculate prior density
+    prior = VonMises(p=True).get(
+        support_space, np.array([180]), [k_vm]
+    )
+
+    # plot
+    plt.fill_between(support_space, prior.squeeze())
+
